49d4df2111b664c6bac3c0fc8608d98452801383
Sophie Gu <sophieqgu@gmail.com>
2021-01-02 03:03:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+let names = ["Lisa", "Kaitlin", "Jan"];
+let ocassion = ["surprise"];
+
+
+function writeCards(names, occasion) {
+  let result = [];
+  for (let i = 0; i < names.length; i++) {
+    result.push(`Thank you, ${names[i]}, for the wonderful ${ocassion} gift!`);
+  }
+  return result;
+}
+writeCards(names, ocassion);
+
+
+let number = 10;
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number--);
+  }
+}
