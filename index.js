06852dfd53e63e19919c02b30e6c4c597b25ad93
Sophie Gu <sophieqgu@gmail.com>
2021-01-02 02:55:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+const name = [""]
+
+function writeCards(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log("Thank you, `${name[0]}`, for ")
+  }
+}
